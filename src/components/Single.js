e7b9a719494a6d7324233d5f06dc793787b3dcaa
mujtabarumi <mujtaba.rumi1@gmail.com>
2021-04-01 10:03:22 +0600

    link reload off

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Single({ val }) {
   return (
     <Col xs="4">
-      <a href={"/play/" + val.id}>
+      <Link to={"/play/" + val.id}>
         <Row>
           <img alt="youtube" src={val.image} />
         </Row>
         <Row>{val.title}</Row>
-      </a>
+      </Link>
     </Col>
   );
 }
